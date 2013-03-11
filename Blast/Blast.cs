0eadac7ba59342d00f43e516044a247feb969332
James Telfer <james@telfer.com>
2013-03-12 09:19:06 +1100

    Shift exception class to own file

--- Blast/Blast.cs
+++ Blast/Blast.cs
@@ -46,19 +46,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace Utils {
-	public class BlastException : Exception {
-		public const string OutOfInputMessage = "Ran out of input before completing decompression";
-		public const string OutputMessage = "Output error before completing decompression";
-		public const string LiteralFlagMessage = "Literal flag not zero or one";
-		public const string DictionarySizeMessage = "Dictionary size not in 4..6";
-		public const string DistanceMessage = "Distance is too far back";
-
-		public BlastException() : base() { }
-		public BlastException(string message) : base(message) { }
-		public BlastException(string message, Exception inner) : base(message, inner) { }
-	}
-
+namespace Utils 
+{
 	public class Blast
 	{
 		public const int MAX_BITS = 13;
